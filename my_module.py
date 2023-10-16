647cd3bdd5ec7846bd110f64407464758ae049bd
ibnerasheed <ibnerasheed@gmail.com>
2023-10-16 22:40:24 +0530

    commit new

---
+++ my_module.py
@@ -0,0 +1,12 @@
+data = {"name": "adil",
+        "subject": ["python", "ds", "chintu", "suresh"],
+        "greetings": "hemlo"
+        }
+
+
+def get_course():
+    return data["subject"]
+
+
+def greetings():
+    return data["greetings"]
---
+++ test1.py
@@ -0,0 +1,15 @@
+def test(a, b):
+    return a + b
+
+
+def test1(b, c):
+    """this is a function that gives multiplication"""
+    return b*c
+
+
+def test3(b, n):
+    return b/n
+
+
+def test4():
+    print("my code written in module test1")
